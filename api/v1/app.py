5ec812d89d581efc21c7c38d561e74b6873abb7c
Nesgomez9 <nicolico99@hotmail.com>
2020-04-29 20:34:35 +0000

    [Add] Adding handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,11 @@ def teardown_storage(self):
     """closes the storage"""
     storage.close()
 
+@app.errorhandler(404)
+def nor_found(error):
+    """handler 404"""
+    return jsonify({"error": "Not found"}), 404
+
 if __name__ == "__main__":
     app.run(host=getenv("HBNB_API_HOST"),
             port=getenv("HBNB_API_PORT"),
