88031d36979035c3cd6337f30ed20b70f0c5b387
Nesgomez9 <nicolico99@hotmail.com>
2020-04-29 20:11:20 +0000

    [Add] Adding stats of the objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,20 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
+Classes = [Amenity, City, Place, Review, State, User]
+
+
 @app_views.route("/status", strict_slashes=False)
 def status():
     """Status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """Return the number of objects of a class"""
+    strings = ["amenities", "cities", "places", "reviews", "states", "users"]
+    dic_count = {}
+    for i in range(len(classes)):
+        dic_count[strings[i]] = storage.count(classes[i])
+    return jsonify(dic_count)
