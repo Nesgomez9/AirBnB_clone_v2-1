6b6f4d647a5be831b1f57c81f63105879d1d213e
sergioq7777 <sergioq.7777@gmail..com>
2020-04-29 20:07:24 -0500

    attempt 5

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -11,9 +11,9 @@ from models.amenity import Amenity
 def get_amenity():
     """Retrieves the list of all Amenity objects: GET"""
     amenity_req = storage.all('Amenity')
-    amenities = amenity_req.values()
+    amenities_all = amenity_req.values()
     amenities_json = []
-    for amenity in amenities:
+    for amenity in amenities_all:
         """to_dict() to serialize an object into valid JSON"""
         amenities_json.append(amenity.to_dict())
     return jsonify(amenities_json)
@@ -22,11 +22,11 @@ def get_amenity():
 @app_views.route('/amenities/<amenity_id>', methods=['GET'])
 def get_id(amenity_id):
     """Retrieves the list of all Amenity objects: GET by id"""
-    amenity = storage.get('Amenity', amenity_id)
+    amenity_all = storage.get('Amenity', amenity_id)
     """If is not linked to any Amenity object, raise a 404 error"""
-    if amenity is None:
+    if amenity_all is None:
         abort(404)
-    return jsonify(amenity.to_dict())
+    return jsonify(amenity_all.to_dict())
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
@@ -45,31 +45,31 @@ def delete_id(amenity_id):
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 def post_amenity():
     """Creates a Amenity: POST"""
-    new_amenity = request.get_json()
-    if new_amenity is None:
+    post_amenity = request.get_json()
+    if post_amenity is None:
         abort(400, 'Not a JSON')
-    if 'name' not in new_amenity:
+    if 'name' not in post_amenity:
         abort(400, 'Missing name')
     """ to transform the HTTP request to a dictionary"""
-    new_amenity = Amenity(name=request.json['name'])
+    post_amenity = Amenity(name=request.json['name'])
     storage.new(new_amenity)
     storage.save()
-    return jsonify(new_amenity.to_dict()), 201
+    return jsonify(post_amenity.to_dict()), 201
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'])
-def put_amenity(amenity_id):
+def update_amenity(amenity_id):
     """Updates a Amenity object: PUT"""
-    req_amenity = request.get_json()
+    put_amenity = request.get_json()
     if not request.json:
         abort(400, 'Not a JSON')
     mod_amenity = storage.get('Amenity', amenity_id)
     if mod_amenity is None:
         abort(404)
-    for key in req_amenity:
+    for key in put_amenity:
         if key == 'id' or key == 'created_at' or key == 'updated_at':
             pass
         else:
-            setattr(mod_amenity, key, req_amenity[key])
+            setattr(mod_amenity, key, put_amenity[key])
     storage.save()
     return jsonify(mod_amenity.to_dict()), 200
